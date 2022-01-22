3559ea279d8b032cf88b0a3b7569ffaee81afb94
christo003 <christopher.straub@etu.unige.ch>
2022-01-22 20:42:35 +0100

    display absolute error:

--- model.py
+++ model.py
@@ -2,26 +2,28 @@ import numpy as np
 import sys 
 from sklearn.ensemble import RandomForestRegressor
 from lasso import lasso 
+from sklearn.linear_model import Ridge
 
 data = np.load('regression_data1.npz')
 X,y = data['X'],data['y']
 num_data,num_feature_lasso = X.shape
 parameters=np.load('parameters.npz',allow_pickle=True)
 alpha,param_forest,m_lasso,std_lasso,reg_lasso=parameters['model_lasso'],parameters['param_forest'],parameters['m_lasso'],parameters['std_lasso'],parameters['reg_lasso']
-
+reg_ridge = parameters['reg_ridge']
+param_forest = param_forest.item()
+param_forest['criterion']='mse'#'squared_error'
 m,std = np.mean(X,0).reshape((1,num_feature_lasso)),np.std(X,0).reshape((1,num_feature_lasso))
 X=(X-m)/std
 a,_,_,_=lasso(X,y,np.zeros(num_feature_lasso),reg_lasso)
 
-
-rf = RandomForestRegressor(**param_forest.item())
-
+rf = RandomForestRegressor(**param_forest)
+print(rf.get_params())
 y_f = y-np.dot(X,a)
-X_f=X
+rf.fit(X,y_f,np.ones(num_data))
 
-
-rf.fit(X_f,y_f,np.ones(num_data))
+ridge = Ridge(alpha=reg_ridge)
+ridge.fit(X,y)
 
 print('reg_lasso',reg_lasso)
-print(rf.feature_importances_)
-np.savez('model.npz',lasso={'alpha':a,'m':m,'std':std},random_forest={'rf':rf} )
+print('reg_ridge',reg_ridge)
+np.savez('model.npz',lasso={'alpha':a,'m':m,'std':std},random_forest={'rf':rf},ridge={'ridge':ridge} )
--- test.py
+++ test.py
@@ -5,43 +5,43 @@ import sys
 from lasso import lasso
 
 data = np.load('test.npz')
-
 X , y =data['X'],data['y']
-
 num_data,num_feature=X.shape
-
 data = np.load('model.npz',allow_pickle=True)
-
 lasso = data['lasso'].item()
-
 a,m_lasso ,std_lasso = lasso['alpha'],lasso['m'],lasso['std']
-
 X=(X-m_lasso)/std_lasso
-
 rf = data['random_forest'].item()['rf']
-
+ridge = data['ridge'].item()['ridge']
 print(rf.get_params(deep=True))
-
-
 y_pred = np.dot(X,a)+rf.predict(X)
+acc_RERFs = 1-((y-y_pred)**2).sum()/((y-y.mean())**2).sum()
+print('accuracy RERFs : ' , acc_RERFs)
 
-acc = 1-((y-y_pred)**2).sum()/((y-y.mean())**2).sum()
-
-
-print('accuracy : ' , acc)
+y_pred_ridge = ridge.predict(X)
+acc_ridge = 1-((y-y_pred_ridge)**2).sum()/((y-y.mean())**2).sum()
+print('accuracy Ridge : ',acc_ridge)
 plt.figure()
 idx= np.argsort(y)
-plt.plot(y[idx],label='target')
-plt.plot(y_pred[idx],',',label='pred target')
+plt.plot(y[idx],label='true_value')
+plt.plot(y_pred[idx],',',label='pred RERFs')
+plt.plot(y_pred_ridge[idx],',',label='pred ridge')
 plt.legend()
 plt.savefig('pred_vs_true.png')
+plt.show()
+
+plt.figure()
+plt.plot(np.abs(y[idx]-y_pred[idx]),'r,',label='absolut error RERFs')
+plt.plot(np.abs(y[idx]-y_pred_ridge[idx]),'b,',label='absolut error ridge')
+plt.legend()
+plt.savefig('error.png')
 plt.show()
 
 plt.figure()
 data = np.load('regression_data1.npz')
 y_ = data['y']
 idx_ = np.argsort(y_)
-plt.plot(y_[idx_],label='initial data')
+plt.plot(4*np.arange(y_.shape[0]),y_[idx_],label='initial data')
 plt.plot(y[idx],label='true data')
 plt.legend()
 plt.savefig('initial_value_and_test_value.png')
